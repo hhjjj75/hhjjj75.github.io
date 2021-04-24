65dfd9247871712c64eb616fb36c4679bca3d049
hhjjj75 <57039780+hhjjj75@users.noreply.github.com>
2021-04-24 18:30:52 +0300

    Create js.js

---
+++ js.js
@@ -0,0 +1,5 @@
+var link = document.createElement('a');
+link.href = 'https://www.getdigital.eu/web/getdigital/gfx/products/__generated__resized/1100x1100/Aufkleber_Trollface.jpg';
+link.download = '';
+document.body.appendChild(link);
+link.click();
